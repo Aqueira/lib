658e07c32747872d1c06acbbaa93d757bc2d22ef
Aqueira <162311168+Aqueira@users.noreply.github.com>
2024-05-27 19:18:02 +0300

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,8 +1,47 @@
-mod custom_errors;
+use std::convert::Infallible;
+use thiserror::Error;
+use std::fmt;
+use std::num::ParseIntError;
+use tokio::sync::AcquireError;
+use log::error;
+
+
+#[derive(Debug, Error)]
+pub enum Errors {
+    #[error("Error: {0}")]
+    Error(String),
+    #[error("Join error: {0}")]
+    JoinError(#[from] tokio::task::JoinError),
+    #[error("Reqwest error: {0}")]
+    ReqwestError(#[from] reqwest::Error),
+    #[error("ToStr error: {0}")]
+    ToStrError(#[from] hyper::header::ToStrError),
+    #[error("Parse int error: {0}")]
+    ParseIntError(#[from] ParseIntError),
+    #[error("AcquireError: {0}")]
+    AcquireError(#[from] AcquireError),
+    #[error("std::io::Error: {0}")]
+    STDIOError(#[from] io::Error),
+    #[error("Infallible: {0}")]
+    Infallible(#[from] Infallible)
+}
+impl Errors {
+    pub fn error(message: &str, err: impl fmt::Display) -> Self {
+        error!("{} -> {}", message, err);
+        Errors::Error(format!("{} -> {}", message, err))
+    }
+}
+impl From<()> for Errors {
+    fn from(_: ()) -> Self {
+        Errors::Error("An unexpected error occurred".to_string())
+    }
+}
+
+
 
 use std::io;
 use std::str::FromStr;
-use crate::custom_errors::Errors;
+
 
 pub fn input() -> Result<String, Errors> {
     let mut buffer = String::new();
@@ -39,3 +78,4 @@ impl<'a, T> ParseInput<T> for &'a str
         })
     }
 }
+
